3c2050c9600116fd24b96c190381aff5ff4f0a09
HiImLawtSimp1e <nqh1703k2@gmail.com>
2024-07-03 14:15:33 +0700

    FE Product EAV (update ProductAttribute action)

--- internship-frontend/src/action/productAttributeAction.ts
+++ internship-frontend/src/action/productAttributeAction.ts
@@ -60,6 +60,66 @@ export const addAttribute = async (
   }
 };
 
+// Define the updateAttribute function
+export const updateAttribute = async (
+  prevState: FormState,
+  formData: FormData
+): Promise<FormState | undefined> => {
+  // Extract the necessary fields from formData
+  const id = formData.get("id") as string;
+  const name = formData.get("name") as string;
+
+  try {
+    const res = await fetch(
+      `http://localhost:5000/api/ProductAttribute/admin/${id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({ id, name }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    if (!res.ok) {
+      // If the response is not OK, parse the error response
+      const errorResponse = await res.json();
+      const { errors } = errorResponse;
+
+      // Create an array to hold error messages
+      let errorMessages: string[] = [];
+
+      // Check if there are specific validation errors and add them to the error messages
+      if (errors) {
+        for (const key in errors) {
+          if (errors.hasOwnProperty(key)) {
+            errorMessages = errorMessages.concat(errors[key]);
+          }
+        }
+      }
+
+      // Return the updated state with errors
+      return { errors: errorMessages };
+    }
+
+    // If the response is OK, parse the response data
+    const responseData: ApiResponse<string> = await res.json();
+    console.log(responseData);
+    const { success, message } = responseData;
+
+    if (success) {
+      // If the response is success, revalidate the path and redirect
+      revalidateTag("selectProductAttribute");
+      revalidateTag("productAttributeList");
+      return { success: true, errors: [] };
+    } else {
+      return { errors: [message] };
+    }
+  } catch (error) {
+    // Handle any unexpected errors
+    console.error("Unexpected error:", error);
+    return { errors: ["An unexpected error occurred. Please try again."] };
+  }
+};
+
 export const deleteAttribute = async (
   prevState: FormState,
   formData: FormData
--- internship-frontend/src/components/dashboard/product-attribute/update-product-attribute-form.tsx
+++ internship-frontend/src/components/dashboard/product-attribute/update-product-attribute-form.tsx
@@ -1,5 +1,89 @@
-const UpdateProductAttributeForm = () => {
-  return <></>;
+"use client";
+
+import { updateAttribute } from "@/action/productAttributeAction";
+import InputField from "@/components/ui/input";
+import { useCustomActionState } from "@/lib/custom/customHook";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+interface IProps {
+  attribute: IProductAttribute;
+}
+
+const UpdateProductAttributeForm = ({ attribute }: IProps) => {
+  const router = useRouter();
+
+  const initialState: FormState = { errors: [] };
+  const [formState, formAction] = useCustomActionState<FormState>(
+    updateAttribute,
+    initialState
+  );
+
+  const [formData, setFormData] = useState({
+    name: attribute.name,
+  });
+
+  const [toastDisplayed, setToastDisplayed] = useState(false);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    formAction(formData);
+    setToastDisplayed(false); // Reset toastDisplayed when submitting
+  };
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    if (formState.errors.length > 0 && !toastDisplayed) {
+      toast.error("Update product attribute failed");
+      setToastDisplayed(true); // Set toastDisplayed to true to prevent multiple toasts
+    }
+    if (formState.success) {
+      toast.success("Updated product attribute successfully!");
+      router.push("/dashboard/product-attributes");
+    }
+  }, [formState, toastDisplayed]);
+
+  return (
+    <form onSubmit={handleSubmit} className="px-4 w-full">
+      <input type="hidden" value={attribute.id} name="id" />
+      <InputField
+        label="Name"
+        id="name"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        required
+      />
+      {formState.errors.length > 0 && (
+        <ul>
+          {formState.errors.map((error, index) => (
+            <li className="text-red-400" key={index}>
+              {error}
+            </li>
+          ))}
+        </ul>
+      )}
+      <button
+        type="submit"
+        className="float-right mt-4 text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+      >
+        Update
+      </button>
+    </form>
+  );
 };
 
 export default UpdateProductAttributeForm;
