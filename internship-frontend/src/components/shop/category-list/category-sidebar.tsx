4c297ba17de219a183cc8181477954cd1cb391ad
HiImLawtSimp1e <nqh1703k2@gmail.com>
2024-07-04 11:21:41 +0700

    1

--- internship-frontend/src/components/shop/category-list/category-sidebar.tsx
+++ internship-frontend/src/components/shop/category-list/category-sidebar.tsx
@@ -7,7 +7,7 @@ interface IProps {
 const CategorySidebar = ({ categories }: IProps) => {
   return (
     <div className="mt-12 h-full transition-transform -translate-x-full sm:translate-x-0">
-      <div className="h-full px-1 py-4 overflow-y-auto bg-gray-50">
+      <div className="h-full px-2 py-4 overflow-y-auto text-xl bg-gray-50 lg:text-base">
         <ul className="space-y-2 font-medium">
           {categories?.map((category: ICategory) => (
             <li key={category.id}>
@@ -16,7 +16,7 @@ const CategorySidebar = ({ categories }: IProps) => {
                   pathname: `/product`,
                   query: { category: category.slug },
                 }}
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                className="flex items-center p-4 text-gray-900 rounded-lg hover:bg-gray-100 group lg:p-2"
               >
                 <span className="flex-1 ms-3 whitespace-nowrap">
                   {category.title}
