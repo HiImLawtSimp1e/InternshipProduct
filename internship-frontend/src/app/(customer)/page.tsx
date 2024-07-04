a70ddf2224e5ded68dae6bd247cb17f47d7c88a0
HiImLawtSimp1e <nqh1703k2@gmail.com>
2024-07-04 11:09:25 +0700

    -1

--- internship-frontend/src/app/(customer)/page.tsx
+++ internship-frontend/src/app/(customer)/page.tsx
@@ -16,18 +16,6 @@ const Products = async () => {
   return <HomeShopProductList products={result} />;
 };
 
-const Categories = async () => {
-  const res = await fetch(`http://localhost:5000/api/Category`, {
-    method: "GET",
-  });
-
-  const categories: ApiResponse<ICategory[]> = await res.json();
-  const { data, success, message } = categories;
-  console.log(data);
-
-  return <ShopCategoryList categories={data} />;
-};
-
 const Home = () => {
   return (
     <div className="bg-gray-100 py-24">
@@ -35,12 +23,6 @@ const Home = () => {
         <h1 className="text-2xl">New Products</h1>
         <Suspense fallback={<Loading />}>
           <Products />
-        </Suspense>
-      </div>
-      <div className="pt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">Categories</h1>
-        <Suspense fallback={<Loading />}>
-          <Categories />
         </Suspense>
       </div>
     </div>
